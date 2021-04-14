b1097a1839eba1b57c4786d68e57d751929745b4
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-04-14 12:56:37 +0100

    update repo to serve images over https

--- src/plugins/blade.js
+++ src/plugins/blade.js
@@ -1,7 +1,7 @@
 module.exports = {
     methods: {
         asset(path) {
-            var base_path = "https://beem.test/storage/" || '';
+            var base_path = "https://thinktech.fuoye360.com/storage/" || '';
             return base_path + path;
         },
     },
